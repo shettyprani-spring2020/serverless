e329a7b3995a51c6563cc72c6af83d661d45ea52
Pranit Shetty <pranitshetty@gmail.com>
2020-03-27 21:46:06 -0400

    waiting for update and email

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-console.log("Invoking Lambda Function");
 let AWS = require("aws-sdk");
 let dynamodb = new AWS.DynamoDB();
 
 exports.handler = async function(event, context, callback) {
+  console.log("Invoking Lambda Function");
   let message = event.Records[0].Sns.Message;
+  message = JSON.parse(message);
   const TO = message.email;
-  const FROM = "no-reply@" + process.env.domainName;
+  const FROM = "no-reply@" + process.env.domain_name;
   const bills = message.bills;
+  console.log("Sending email from " + FROM + " TO: " + TO);
   let db_params = {
     Key: {
       id: { S: TO }
@@ -16,29 +18,26 @@ exports.handler = async function(event, context, callback) {
   let result = await dynamodb.getItem(db_params).promise();
   console.log(result);
   let now = Date.now();
-  if (result == undefined) {
+  if (Object.keys(result).length === 0) {
     const ttl_time = Date.now() / 1000 + 60 * 60;
-    const data = {
+    const db_data = {
       TableName: "emaildata",
       Item: {
         id: { S: TO },
-        TTL: { N: ttl_time }
+        TTL: { N: ttl_time.toString() }
       }
     };
-    dynamodb.putItem(data, function(err, data) {
-      if (err) {
-        console.log("Error", err);
-      } else {
-        console.log("Success", data);
-      }
-    });
+    result = await dynamodb.putItem(db_data).promise();
+    console.log(result);
   } else {
     const last_sent = result.Item.TTL;
+    console.log("Item exists!");
     if (last_sent >= now) {
+      console.log("Email sent within the last 1 hour! \nNot resending!");
       return;
     }
   }
-
+  console.log("Sending an email!");
   let email_params = {
     Destination: {
       ToAddresses: [TO]
@@ -62,7 +61,7 @@ exports.handler = async function(event, context, callback) {
     },
     Source: FROM
   };
-  let sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+  let sendPromise = await new AWS.SES({ apiVersion: "2010-12-01" })
     .sendEmail(email_params)
     .promise();
 
